e59df23bb73898f512d923b84ab34fc8580faac4
sanchita <sanchitamishra0726@gmail.com>
2026-01-05 21:29:38 +0530

    Added array problems: linear search, binary search, reverse array

---
+++ JavaPractice.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class JavaPractice {
+
+    public static int factorial(int n){
+        int fact  = 1;
+       for(int i=1; i<=n; i++){
+         fact = fact*i;
+       }
+       return fact;
+    }
+    public static void main(String[] args) {
+    System.out.println(factorial(7));
+      }
+        }
+        
+        
+    
+
